deb919f852ccabfef16307b9942a5c3eeb827f3b
Semen-Gomzyak <Blacklife1817@gmail.com>
2022-12-11 05:19:42 +0100

    change home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import BoxSx from 'components/Box/Box';
 
 const Home = () => (
     <BoxSx>
-        <h1>Welcome to contact storage app. Register and create your phone book</h1>
+        <h1>Welcome to the contact storage app. Register and create your phone book. If you have an account login, please log in</h1>
     
     </BoxSx>
 );
